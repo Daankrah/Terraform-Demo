6b01ef2b7a9fb47cf6a9d50de343d8ac979f2bbd
Daankrah <115485003+Daankrah@users.noreply.github.com>
2022-10-11 13:50:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "Dan-Terraform-Demos"
+    organization = "EnterOrganizationHere"
 
     workspaces {
-      name = "demo-github-actions"
+      name = "EnterWorkspaceHere"
     }
   }
 }
